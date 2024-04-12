fe6b8b6129b22631abac55794b410c975871498b
nextit <nextitcenter@gmail.com>
2024-04-12 09:16:52 +0900

    필터에서 한글 인코딩 처리

--- src/main/java/member/MemberInsertServlet.java
+++ src/main/java/member/MemberInsertServlet.java
@@ -26,7 +26,6 @@ public class MemberInsertServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
